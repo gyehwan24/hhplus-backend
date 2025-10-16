d285eda4385f1cc481667e23957a5ddb9ed7ae74
b99hwan <b99hwan@incross.com>
2025-10-16 18:04:21 +0900

    feat: implement Concert Repository (DIP) - ConcertRepository - ConcertScheduleRepository - ScheduleSeatRepository

---
+++ server-java/src/main/java/kr/hhplus/be/server/concert/domain/repository/ScheduleSeatRepository.java
@@ -0,0 +1,12 @@
+package kr.hhplus.be.server.concert.domain.repository;
+
+import kr.hhplus.be.server.concert.domain.ScheduleSeat;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ScheduleSeatRepository {
+    List<ScheduleSeat> findByScheduleId(Long scheduleId);
+    Optional<ScheduleSeat> findById(Long id);
+    List<ScheduleSeat> findAllByIdWithLock(List<Long> ids);
+}
---
+++ server-java/src/main/java/kr/hhplus/be/server/concert/infrastructure/persistence/ScheduleSeatRepositoryImpl.java
@@ -0,0 +1,31 @@
+package kr.hhplus.be.server.concert.infrastructure.persistence;
+
+import kr.hhplus.be.server.concert.domain.ScheduleSeat;
+import kr.hhplus.be.server.concert.domain.repository.ScheduleSeatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class ScheduleSeatRepositoryImpl implements ScheduleSeatRepository {
+
+    private final ScheduleSeatJpaRepository jpaRepository;
+
+    @Override
+    public List<ScheduleSeat> findByScheduleId(Long scheduleId) {
+        return jpaRepository.findByScheduleId(scheduleId);
+    }
+
+    @Override
+    public Optional<ScheduleSeat> findById(Long id) {
+        return jpaRepository.findById(id);
+    }
+
+    @Override
+    public List<ScheduleSeat> findAllByIdWithLock(List<Long> ids) {
+        return jpaRepository.findAllByIdWithLock(ids);
+    }
+}
